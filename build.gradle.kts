5afa5307d56787cb76a3449608f95157569bffb0
James Ward <james@jamesward.com>
2019-12-06 07:27:23 -0700

    bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,10 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.allopen.gradle.AllOpenExtension
 
 plugins {
     application
-    kotlin("jvm") version "1.3.21"
-    kotlin("kapt") version "1.3.21"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.3.21"
+    kotlin("jvm") version "1.3.61"
+    kotlin("kapt") version "1.3.61"
     id("com.google.cloud.tools.jib") version "1.0.2"
     id("com.github.johnrengelman.shadow") version "4.0.4"
 }
@@ -21,25 +17,16 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib"))
-    compile(kotlin("reflect"))
 
-    compile("io.micronaut:micronaut-runtime:1.1.0")
-    compile("io.micronaut:micronaut-http-client:1.1.0")
-    compile("io.micronaut:micronaut-http-server-netty:1.1.0")
-    compile("io.micronaut:micronaut-views:1.1.0")
+    compile("io.micronaut:micronaut-runtime:1.2.7")
+    compile("io.micronaut:micronaut-http-server-netty:1.2.7")
+    compile("io.micronaut:micronaut-views:1.2.0")
     compile("ch.qos.logback:logback-classic:1.2.3")
 
     runtime("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
     runtime("org.thymeleaf:thymeleaf:3.0.11.RELEASE")
 
-    kapt("io.micronaut:micronaut-inject-java:1.1.0")
-    kapt("io.micronaut:micronaut-validation:1.1.0")
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.3.21")
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.1")
-    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.1")
-
-    kaptTest("io.micronaut:micronaut-inject-java:1.1.0")
+    kapt("io.micronaut:micronaut-inject-java:1.2.7")
 }
 
 java {
@@ -55,30 +42,6 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClassName = "hello.WebAppKt"
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform {
-        includeEngines("spek2")
-    }
-    testLogging {
-        showStandardStreams = true
-        exceptionFormat = TestExceptionFormat.FULL
-        events(TestLogEvent.STARTED, TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
-    }
-}
-
-configure<AllOpenExtension> {
-    annotation("io.micronaut.aop.Around")
-}
-
-tasks.all {
-    when(this) {
-        is JavaForkOptions -> {
-            jvmArgs("-noverify")
-            jvmArgs("-XX:TieredStopAtLevel=1")
-        }
-    }
 }
 
 jib {
