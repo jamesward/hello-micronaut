1eb3405590c72ae2e0832d31d7bebf6ce63e0b06
Jean-Michel Fayard <jmfayard@gmail.com>
2019-12-09 12:06:03 +0100

    gradle: replace compile vs implementation
    the compile configuration is deprecated
    "api" or "runtime" should now be used
    For why, see
    https://docs.gradle.org/current/userguide/java_library_plugin.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,15 +16,15 @@ repositories {
 }
 
 dependencies {
-    compile(kotlin("stdlib"))
+    implementation(kotlin("stdlib"))
 
-    compile("io.micronaut:micronaut-runtime:1.2.7")
-    compile("io.micronaut:micronaut-http-server-netty:1.2.7")
-    compile("io.micronaut:micronaut-views:1.2.0")
-    compile("ch.qos.logback:logback-classic:1.2.3")
+    implementation("io.micronaut:micronaut-runtime:1.2.7")
+    implementation("io.micronaut:micronaut-http-server-netty:1.2.7")
+    implementation("io.micronaut:micronaut-views:1.2.0")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
 
-    runtime("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
-    runtime("org.thymeleaf:thymeleaf:3.0.11.RELEASE")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
+    runtimeOnly("org.thymeleaf:thymeleaf:3.0.11.RELEASE")
 
     kapt("io.micronaut:micronaut-inject-java:1.2.7")
 }
